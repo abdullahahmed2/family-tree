58bcceb6ea1afe66cd462a4dc705005baf7cc119
UsmanRahman <41521342+UsmanRahman@users.noreply.github.com>
2020-07-08 13:36:52 -0400

    Updated App.js file
    Includes code that renders about page, contact page, and error404 page

--- family-tree-project/family-tree/src/App.js
+++ family-tree-project/family-tree/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
+import ContactPage from './pages/contactpage.js';
+import AboutPage from './pages/aboutpage.js';
+import NotFound from './pages/notfoundpage.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+            <Switch>
+                <Route exact path='/' component={ContactPage} />
+                <Route exact path='/about' component={AboutPage} />
+                <Route component={NotFound} />
+            </Switch>
+        </Router>
     </div>
   );
 }
