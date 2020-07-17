c8d2010228badef40a40dbc76abbf1e881a8b812
Abdullah Suhayb Ahmed <aahmed@Abdullah-Suhayb-Ahmed.local>
2020-07-17 21:29:47 +0500

    registration page

--- family-tree-project/family-tree/src/App.js
+++ family-tree-project/family-tree/src/App.js
@@ -4,19 +4,21 @@ import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-ro
 import ContactPage from './pages/contactpage.js';
 import AboutPage from './pages/aboutpage.js';
 import NotFound from './pages/notfoundpage.js';
-import NavBar from './pages/nav.js';
+import RCName from './RCName.js';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
         <Router>
             <Switch>
-                <Route exact path="/contact" component={ContactPage} />
-                <Route exact path="/about" component={AboutPage} />
-                <Route exact path="/404" component={NotFound} />
-                <Redirect to="/404" />
+                <Route exact path='/' component={ContactPage} />
+                <Route exact path='/about' component={AboutPage} />
+                <Route exact path='/name' component={RCName} />
+                <Route component={NotFound} />
             </Switch>
         </Router>
+      </div>
     </div>
   );
 }
