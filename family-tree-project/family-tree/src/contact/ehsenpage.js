51463d45249f2ebafd7b920886d449560758c930
EhsenA <esn416@gmail.com>
2020-07-22 16:24:47 -0700

    updated ehsenpage

--- family-tree-project/family-tree/src/contact/ehsenpage.js
+++ family-tree-project/family-tree/src/contact/ehsenpage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import ReactTypingEffect from 'react-typing-effect';
 class ehsenpage extends React.Component {
   render (){
     return(
       <div>
-        <h1>Ehsen</h1>
+        <h1>Ehsen Ahmad</h1>
+        <ReactTypingEffect text={['Mechanical Engineering student at Northeastern University | CS Enthusiast']}  speed={50} eraseDelay={1000000}/>
+<br></br>
+<p>blah blah blah</p>
       </div>
     )
   }
