8bd51c73514fd4bb095fb2985c6daf27c3248886
sofiaaleem1 <sofiaaleem@gmail.com>
2020-07-26 19:17:39 -0400

    commit

--- family-tree-project/family-tree/src/pages/historicalplaces.js
+++ family-tree-project/family-tree/src/pages/historicalplaces.js
@@ -6,9 +6,22 @@ class history extends React.Component {
     return(
       <div>
         <div><NavBar/> </div>
-        <h1>Hello</h1>
+        
+        <h1>Historical Places</h1>
+        <strong>Patna</strong>
+        <p> Patna is the capital city of the province Bihar, and is one of the
+        oldest conintuously inhabited places in the world. It was founded in
+        490 BCE under the name of Pataliputra and borders the Ganges River.
+        Patna was the center of a lot of art and education and has been
+        associated with two ancient religions, Buddhism and Jainism. This
+        city has played a big role in India historically, being a significant
+        territory during the Delhi Sultanate, Mughul Empire, Nawabs of
+        Bengal, East India Company, and the British Raj. Patna was the
+        center of the First War of Independence and actively participated
+        in India's independence movement.</p>
 
-
+        <strong>Sherghati</strong>
+        <p> </p>
 
       </div>
     )
