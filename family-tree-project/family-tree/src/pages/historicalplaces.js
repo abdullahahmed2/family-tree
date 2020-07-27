86ae049511e2feda2e817b57a123c8be9f3b8999
sofiaaleem1 <sofiaaleem@gmail.com>
2020-07-26 21:22:31 -0400

    commit

--- family-tree-project/family-tree/src/pages/historicalplaces.js
+++ family-tree-project/family-tree/src/pages/historicalplaces.js
@@ -6,9 +6,14 @@ class history extends React.Component {
     return(
       <div>
         <div><NavBar/> </div>
-        
+
         <h1>Historical Places</h1>
-        <strong>Patna</strong>
+        <body>
+
+        <h3>Patna</h3>
+
+        <img scr="Patna.jpg" alt="Picture of Patna" />
+
         <p> Patna is the capital city of the province Bihar, and is one of the
         oldest conintuously inhabited places in the world. It was founded in
         490 BCE under the name of Pataliputra and borders the Ganges River.
@@ -19,10 +24,22 @@ class history extends React.Component {
         Bengal, East India Company, and the British Raj. Patna was the
         center of the First War of Independence and actively participated
         in India's independence movement.</p>
+        <br></br>
 
-        <strong>Sherghati</strong>
-        <p> </p>
+        <h3>Sherghati</h3>
 
+        <img scr="" alt="Picture of Sherghati"/>
+
+        <p> Sherghati is a town in Bihar, India in the Gaya district and is
+        surrounded by the Morhar River. It translates to “The Valley of Lions”
+        to honor the brave people that lived there. It was under the rule of the
+        British Raj until in 1857 the monarch, Raja Jehangir Khan Bux, revolted
+        against the British. In 1865, a meteorite from Mars fell in this town
+        which was being kept in a London museum and is called the
+        “Shergotty meteorite”. Their main industry is agriculturally based, the
+        major crops grown are rice, wheat, legume, groundnut, and sugarcane.</p>
+
+        </body>
       </div>
     )
   }
