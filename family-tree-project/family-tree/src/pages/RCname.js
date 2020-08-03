fb75de4fcdc3eeb0f1bceab3d93bccb4a83e3ec4
mabdulrahmanahmed@gmail.com <mabdulrahmanahmed@gmail.com>
2020-08-03 16:31:31 +0500

    .

--- family-tree-project/family-tree/src/pages/RCname.js
+++ family-tree-project/family-tree/src/pages/RCname.js
@@ -1,53 +1,65 @@
 import React from 'react';
-import './RCName.css';
-import NavBar from './pages/nav.js';
+import './RCname.css';
+import NavBar from './nav.js';
 
 class RCName extends React.Component {
   render() {
     return(
-     <body>
-     <NavBar />
-      <div className="register">
-        <h1>Register Here</h1>
-        <form method='post' id='register' action=''>
-            <label for='name'>First Name:</label><br />
-            <input type='text' name='Firstname' id='name' maxLength={10} placeholder='First name' required/><br /><br />
 
-            <label for='name'>Middle Name:</label><br />
-            <input type='text' name='Middlename' id='name' maxLength={10} placeholder='Middle name' required/><br /><br />
+       <body>
 
-            <label for='name'>Last Name:</label><br />
-            <input type='text' name='Lastname' id='name' maxLength={10} placeholder='Last name' required/><br /><br />
+        <div className="container">
+            <NavBar/>
 
-            <label for='dob'>Date of Birth:</label><br/>
-            <input type='date' name='date-of-birth' id='dob' placeholder='mm/dd/yyyy' required /><br /><br />
+          <form method='post' id='register' action=''>
 
-            <label>Mobile number: </label><br />
-            <select id='phone'>
-              <option>+92</option>
-              <option>+1</option>
-            </select>
-            <input type='number' placeholder='Mobile number' name='Mobile number' id='Mnum' maxLength={10} required ></input><br /><br />
+              <h1> Register </h1>
 
-            <label>Email: </label><br />
-            <input type='Email' name='email' id='name' placeholder='example@gmail.com' required></input><br /><br />
+              <label for='Fname'>Name:</label><br/>
+              <input type='text' name='Firstname' id='Fname' maxLength={10} placeholder='First name' required/>
 
-            <label>Password: </label><br />
-            <input type='Password' name='pass' id='name' placeholder='password' required></input><br /><br />
+              <input type='text' name='Middlename' id='Mname' maxLength={10} placeholder='Middle name' required/>
 
-            <label>Re-enter your password:</label><br />
-            <input type='Password' name='pass' id='name' placeholder='Re-enter password' required></input><br /><br />
-
-            <label className='gender'>Male:</label>&nbsp;
-            <input type='radio' id='gender'></input>&nbsp;&nbsp;
-            <label className='gender'>Female:</label>&nbsp;
-            <input type='radio' id='gender'></input><br /><br />
+              <input type='text' name='Lastname' id='Lname' maxLength={10} placeholder='Last name' required/><br/><br/>
 
 
-            <input type="submit" value="Submit" className="submit"/>
-        </form>
-      </div>
-     </body>
+
+              <label for='dob'>Date of Birth:</label><br/>
+              <input type='date' name='date-of-birth' id='dob' placeholder='mm/dd/yyyy' required/> <br/><br/>
+
+
+              <label for="Mnum">Mobile number: </label><br/>
+              <select id='phone'>
+                <option>+92</option>
+                <option>+1</option>
+              </select>
+              <input type='number' placeholder='Mobile number' name='Mobile number' id='Mnum' maxLength={10} required ></input><br />
+
+
+
+              <label for="email">Email: </label><br />
+              <input type='Email' name='email' id='email' placeholder='example@gmail.com' required></input><br /><br />
+
+
+
+              <label for="password">Password: </label><br />
+              <input type='Password' name='pass' id='password' placeholder='password' required></input><br /><br />
+
+              <label for="Rpassword">Re-enter password:</label><br />
+              <input type='Password' name='pass' id='Rpassword' placeholder='Re-enter password' required></input><br /><br />
+
+
+
+              <label className='gender'>Male:</label>&nbsp;
+              <input type='radio' id='gender'></input>&nbsp;&nbsp;
+              <label className='gender'>Female:</label>&nbsp;
+              <input type='radio' id='gender'></input><br /><br />
+
+
+              <input type="submit" value="Submit" className="submit"/>
+          </form>
+        </div>
+       </body>
     );
   }
 }
