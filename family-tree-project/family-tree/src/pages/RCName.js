6dd8ac0be7e857a4a72b6852d05d389db2c87922
Usama Ameen Ahmed <usamaameen@Usamas-MacBook-Pro.local>
2020-08-03 17:35:56 +0500

    Corrected radio button

--- family-tree-project/family-tree/src/pages/RCName.js
+++ family-tree-project/family-tree/src/pages/RCName.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './RCName.css';
-import NavBar from './pages/nav.js';
+import NavBar from './nav.js';
 
 class RCName extends React.Component {
   render() {
@@ -20,14 +20,14 @@ class RCName extends React.Component {
             <input type='text' name='Lastname' id='name' maxLength={10} placeholder='Last name' required/><br /><br />
 
             <label for='dob'>Date of Birth:</label><br/>
-            <input type='date' name='date-of-birth' id='dob' placeholder='mm/dd/yyyy' required /><br /><br />
+            <input type='date' name='date-of-birth' id='dob' placeholder='dd/mm/yyyy' required /><br /><br />
 
             <label>Mobile number: </label><br />
             <select id='phone'>
               <option>+92</option>
               <option>+1</option>
             </select>
-            <input type='number' placeholder='Mobile number' name='Mobile number' id='Mnum' maxLength={10} required ></input><br /><br />
+            <input type='tel' placeholder='Mobile number' name='Mobile number' id='Mnum' maxLength={10} required ></input><br /><br />
 
             <label>Email: </label><br />
             <input type='Email' name='email' id='name' placeholder='example@gmail.com' required></input><br /><br />
@@ -38,10 +38,10 @@ class RCName extends React.Component {
             <label>Re-enter your password:</label><br />
             <input type='Password' name='pass' id='name' placeholder='Re-enter password' required></input><br /><br />
 
-            <label className='gender'>Male:</label>&nbsp;
-            <input type='radio' id='gender'></input>&nbsp;&nbsp;
-            <label className='gender'>Female:</label>&nbsp;
-            <input type='radio' id='gender'></input><br /><br />
+            <label for='gender'>Male:</label>&nbsp;
+            <input type="radio" id="male" name="gender" value="male"></input>&nbsp;
+            <label for= 'Female'>Female:</label>&nbsp;
+            <input type="radio" id="female" name="gender" value="female"></input><br /><br />
 
 
             <input type="submit" value="Submit" className="submit"/>
