3a05fa4fc1e4675d93549a3c16a3d24036a484f4
sofiaaleem1 <sofiaaleem@gmail.com>
2020-08-04 21:40:39 -0400

    commit

--- family-tree-project/family-tree/src/pages/historicalplaces.js
+++ family-tree-project/family-tree/src/pages/historicalplaces.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header.js';
 import Footer from './Footer.js';
+import Collapsible from 'react-collapsible';
 
 class history extends React.Component {
   render() {
@@ -11,7 +12,7 @@ class history extends React.Component {
         <h1>Historical Places</h1>
         <body>
 
-        <h3>Patna</h3>
+        <li><Collapsible trigger="Patna">
         <img id="p1" src="/images/patnaimage1.jpg" alt="Patna during British Occupation" width= "390" height= "300" />
         <img id="p3" src="/images/patnaimage3.jpg" alt="Women surround Religious Monument" width= "300" height= "370"/>
         <img id="p4" src="/images/patnaimage4.jpg" alt="Map of Patna" width= "370" height= "300"/>
@@ -24,22 +25,24 @@ class history extends React.Component {
         Bengal, East India Company, and the British Raj. Patna was the
         center of the First War of Independence and actively participated
         in India's independence movement. </p>
+            </Collapsible></li>
 
-        <h3>Sherghati</h3>
 
+            <li><Collapsible trigger="Sherghati">
+            <img id="s1" src="/images/sherghatiimage1.jpg" alt="Picture of Sherghati's landscape" width= "350" height= "300"/>
+            <img id="s2" src="/images/sherghatiimage2.jpg" alt="Picture of Shergotty meteorite" width= "350" height= "330"/>
+            <img id="s3" src="/images/sherghatiimage3.jpg" alt="Picture of a drawing of a hills in Sherghati" width= "350" height= "300"/>
 
-        <img id="s1" src="/images/sherghatiimage1.jpg" alt="Picture of Sherghati's landscape" width= "350" height= "300"/>
-        <img id="s2" src="/images/sherghatiimage2.jpg" alt="Picture of Shergotty meteorite" width= "350" height= "330"/>
-        <img id="s3" src="/images/sherghatiimage3.jpg" alt="Picture of a drawing of a hills in Sherghati" width= "350" height= "300"/>
-
-        <p> Sherghati is a town in Bihar, India in the Gaya district and is
-        surrounded by the Morhar River. It translates to “The Valley of Lions”
-        to honor the brave people that lived there. It was under the rule of the
-        British Raj until in 1857 the monarch, Raja Jehangir Khan Bux, revolted
-        against the British. In 1865, a meteorite from Mars landed in Sherghati,
-        but was confiscated by colonisers and resides in a London museum as the
-        “Shergotty meteorite”. Their main industry is agriculturally based, the
-        major crops grown are rice, wheat, legume, groundnut, and sugarcane.</p>
+            <p> Sherghati is a town in Bihar, India in the Gaya district and is
+            surrounded by the Morhar River. It translates to “The Valley of Lions”
+            to honor the brave people that lived there. It was under the rule of the
+            British Raj until in 1857 the monarch, Raja Jehangir Khan Bux, revolted
+            against the British. In 1865, a meteorite from Mars landed in Sherghati,
+            but was confiscated by colonisers and resides in a London museum as the
+            “Shergotty meteorite”. Their main industry is agriculturally based, the
+            major crops grown are rice, wheat, legume, groundnut, and sugarcane.</p>
+                </Collapsible></li>
+    
 
         </body>
 
