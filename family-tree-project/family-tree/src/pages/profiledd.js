9835c64c6b441502ad1d6f0219e8d61b253dad3c
Usama Ameen Ahmed <usamaameen@Usamas-MacBook-Pro.local>
2020-08-08 15:19:12 +0500

    Dropdown toggle added

--- family-tree-project/family-tree/src/pages/profiledd.js
+++ family-tree-project/family-tree/src/pages/profiledd.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import  './profiledd.css';
-import Testmodal from './testmodal.js';
+import "./profiledd.css";
 
- class Profiledd extends React.Component {
-	render() {
-	    return (
-	      <div className="profiledd">
-	        <div className="container">
-	          <button type="button" className="button">
-	            ☰
-	          </button>
-			  <div class="dropdown">
-				  <ul>
-					<li>Your Profile</li>
-					<li>Edit Profile</li>
-					<li>Logout</li>
-				  </ul>
-			  </div>
-	        </div>
-	      </div>
-          
-	    );
-	  }
+class Profiledd extends React.Component {
+  container = React.createRef();
+  state = {
+    open: false,
+  };
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside);
+  }
+   handleClickOutside = event => {
+    if (this.container.current && !this.container.current.contains(event.target)) {
+      this.setState({
+        open: false,
+      });
+    }
+  };
+  handleButtonClick = () => {
+    this.setState(state => {
+      return {
+        open: !state.open,
+      };
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="container" ref={this.container}>
+          <button type="button" class="button" onClick={this.handleButtonClick}>
+            ☰
+          </button>
+          {this.state.open && (
+            <div class="container" id="list">
+              <ul>
+                <li>Your Profile</li>
+                <li>Edit Profile</li>
+                <li>Logout</li>
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
-export default Profiledd
+
+export default Profiledd;
