bd871b51ab92057dad3f0e6e339b9c2fce276df2
UsmanRahman <usmansrahman1@gmail.com>
2020-08-11 11:22:06 -0400

    updated about page to JSON objects

--- family-tree-project/family-tree/src/pages/aboutpage.js
+++ family-tree-project/family-tree/src/pages/aboutpage.js
@@ -4,7 +4,7 @@ import Footer from './Footer.js';
 import { Link } from 'react-router-dom';
 import './pages.css';
 import Collapsible from 'react-collapsible';
-import ehsenpage from './ehsenpage.js';
+import testData from './aboutpage-content.json';
 
 class AboutPage extends React.Component {
     render() {
@@ -22,63 +22,12 @@ class AboutPage extends React.Component {
                     </ul>
                     <ul id="project-team">
                     <b>Project Team</b>:
-
-                        <li><Collapsible trigger="Ehsen Ahmad">
-                            <p>Ehsen is a first-year engineering student at Northeastern. He enjoys learning different CS languages and applying them in mini projects. Ehsen contributed to the code aspect of our application.</p>
-                            <a href="https://www.linkedin.com/in/ehsen-ahmad-7374921b3//" target="_blank">Connect with Ehsen on LinkedIn!</a>
-                            </Collapsible></li>
-
-                        <li><Collapsible trigger="Abdullah Ahmed">
-                            <p>Abdullah Ahmed</p>
-                            <a href="http://linkedin.com/in/abdullah-ahmed-3698251b0" target="_blank">Connect with Abdullah on LinkedIn!</a>
-                            </Collapsible></li>
-
-                        <li><Collapsible trigger="Muhammad Ahmed">
-                            <p>Muhammad Ahmed</p>
-                            <a href="https://www.linkedin.com/" target="_blank">Connect with Muhammad on LinkedIn!</a>
-                            </Collapsible> </li>
-
-                        <li><Collapsible trigger="Syed Fahad Ahmed">
-                            <p>Adaab!<br></br>
-Syed Fahad Ahmed is one of our main coders of Sadat-E-Janjneri.
-He is a passionate individual who is currently completing his last year of Alevel’s as pre-engineering student in Karachi.
-His interest lies within the Artificial Intelligence ambit and thrives to excel in his desired discipline. His contribution to this project has been consistent and remarkable and surely irreplaceable.</p>
-                            <a href="https://www.linkedin.com/in/fahad-ahmed-b6b5a31b3/" target="_blank">Connect with Syed on LinkedIn!</a>
-                            </Collapsible> </li>
-
-                        <li><Collapsible trigger="Usama Ahmed">
-                            <p>Usama Ahmed</p>
-                            <a href="https://linkedin.com/in/usama-ameen-ahmed-4501001b1" target="_blank">Connect with Usama on LinkedIn!</a>
-                            </Collapsible> </li>
-
-                        <li><Collapsible trigger="Yusra Ahmed">
-                            <p>Salam!<br></br>
-Yusra Ahmed is an ambitious individual who is currently pursuing Chartered Accountancy. With her prior experience of marketing and advertising, her contribution to the project is to advertise and coordinate to ensure that Sadat-E-Janjneri is dealt professionally and reaches the audience in a pleasing manner. Moreover, with expertise in finance and managing, she aims to add value to our project.</p>
-                            <a href="http://linkedin.com/in/yusra-ahmed-741504196" target="_blank">Connect with Yusra on LinkedIn!</a>
-                            </Collapsible> </li>
-
-                        <li><Collapsible trigger="Hafsa Aleem">
-                          <div>
-                          <p>Hafsa is a high school student who worked alongside her team members to build this family-oriented application. She picked up many new skills, such as coding, to help contribute to this project. Hafsa is eager to continue gaining experience in different fields in order to determine her future work pathway.</p>
-                            <a href="https://www.linkedin.com/in/hafsa-aleem-773358a5/" target="_blank">Connect with Hafsa on LinkedIn!</a>
-                          </div>
-                          </Collapsible></li>
-
-                        <li><Collapsible trigger="Sofia Aleem">
-                            <p>Sofia Aleem</p>
-                            <a href="https://www.linkedin.com/in/sofia-aleem-8b55a11a0/" target="_blank">Connect with Sofia on LinkedIn!</a>
-                            </Collapsible> </li>
-
-                        <li><Collapsible trigger="Ramiz Hassan">
-                            <p>Ramiz Hassan</p>
-                            <a href="https://www.linkedin.com/in/syed-ramiz-hassan-3549721b3/" target="_blank">Connect with Ramiz on LinkedIn!</a>
-                            </Collapsible> </li>
-
-                        <li><Collapsible trigger="Usman Rahman">
-                            <p>UConn CS Student</p>
-                            <a href="https://www.linkedin.com/in/usman-rahman-6a6086161/" target="_blank">Connect with Usman on LinkedIn!</a>
-                            </Collapsible> </li>
-
+                        {testData.map(data => (
+                                <li><Collapsible trigger = {data.name}>
+                                    <p>{data.description}</p>
+                                    <a href={data.LinkedIn} target="_blank">Connect with {data.name} on LinkedIn!</a>
+                                </Collapsible></li>
+                        ))}
                           <Footer/>
                     </ul>
                 </div>
