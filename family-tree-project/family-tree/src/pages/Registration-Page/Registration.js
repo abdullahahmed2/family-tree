9eb95755bbb6a6ec849d01403df55964c3e16dbb
Usama Ameen Ahmed <usamaameen@Usamas-MacBook-Pro.local>
2020-08-14 16:12:59 +0500

    .

--- family-tree-project/family-tree/src/pages/Registration-Page/Registration.js
+++ family-tree-project/family-tree/src/pages/Registration-Page/Registration.js
@@ -20,18 +20,41 @@ class RCName extends React.Component {
 
               <div className="name">
                 <label for='Fname'>Name:</label><br/>
-                <input type='text' name='Firstname' id='Fname' maxLength={10} placeholder='First name' required/>
+                <input
+                    type='text'
+                    name='Firstname'
+                    id='Fname'
+                    maxLength={10}
+                    placeholder='First name'
+                    required/>
 
-                <input type='text' name='Middlename' id='Mname' maxLength={10} placeholder='Middle name' required/>
+                <input
+                    type='text'
+                    name='Middlename'
+                    id='Mname'
+                    maxLength={10}
+                    placeholder='Middle name'
+                    required/>
 
-                <input type='text' name='Lastname' id='Lname' maxLength={10} placeholder='Last name' required/><br/><br/>
+                <input
+                    type='text'
+                    name='Lastname'
+                    id='Lname'
+                    maxLength={10}
+                    placeholder='Last name'
+                    required/><br/><br/>
              </div>
 
 
 
              <div className= "DOB">
                 <label for='dob'>Date of Birth:</label><br/>
-                <input type='date' name='date-of-birth' id='dob' placeholder='mm/dd/yyyy' required/> <br/><br/>
+                <input
+                    type='date'
+                    name='date-of-birth'
+                    id='dob'
+                    placeholder='mm/dd/yyyy'
+                    required/> <br/><br/>
              </div>
 
 
@@ -42,25 +65,46 @@ class RCName extends React.Component {
                   <option>+92</option>
                   <option>+1</option>
                 </select>
-                <input type='tel' placeholder='Mobile number' name='Mobile number' id='Mnum' maxLength={10} required ></input><br/><br/>
+                <input
+                    type='tel'
+                    placeholder='Mobile number'
+                    name='Mobile number'
+                    id='Mnum'
+                    maxLength={10}
+                    required ></input><br/><br/>
            </div>
 
 
 
            <div className="Email">
                 <label for="email">Email: </label><br />
-                <input type='Email' name='email' id='email' placeholder='example@gmail.com' required></input><br /><br />
+                <input
+                    type='Email'
+                    name='email'
+                    id='email'
+                    placeholder='example@gmail.com'
+                    required></input><br /><br />
            </div>
 
 
 
            <div className="Password">
                 <label for="password">Password: </label><br />
-                <input type='Password' name='pass' id='password' placeholder='password' required></input><br /><br />
+                <input
+                    type='Password'
+                    name='pass'
+                    id='password'
+                    placeholder='password'
+                    required></input><br /><br />
 
 
                 <label for="Rpassword">Repeat password:</label><br />
-                <input type='Password' name='pass' id='Rpassword' placeholder='Re-enter password' required></input> <br/> <br/><br/><br/>
+                <input
+                    type='Password'
+                    name='pass'
+                    id='Rpassword'
+                    placeholder='Re-enter password'
+                    required></input> <br/> <br/><br/><br/>
            </div>
 
 
@@ -72,16 +116,27 @@ class RCName extends React.Component {
               </div>
 
                <label for="gender">Male</label>&nbsp;
-               <input type="radio" id="male" name="gender" value="male"></input>&nbsp; <br/>
+               <input
+                   type="radio"
+                   id="male"
+                   name="gender"
+                   value="male"></input>&nbsp; <br/>
 
                <label for= "Female">Female</label>&nbsp;
-               <input type="radio" id="female" name="gender" value="female"></input> <br/><br/>
+               <input
+                   type="radio"
+                   id="female"
+                   name="gender"
+                   value="female"></input> <br/><br/>
 
           </div>
 
 
           <div className="Submit">
-             <input type="submit" value="Submit" className="submit" />
+             <input
+                 type="submit"
+                 value="Submit"
+                 className="submit" />
           </div>
          </form>
        </div>
