b3a52c695f68fd83645e6d094f5b9b9199922aa9
mabdulrahmanahmed <mabdulrahmanahmed@gmail.com>
2020-08-14 20:58:12 +0500

    rearranged imports and routes

--- family-tree-project/family-tree/src/App.js
+++ family-tree-project/family-tree/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
+
+import homepage from './pages/Homepage/Homepage.js';
+import Login from './pages/Login-Page/loginpage.js';
 import ContactPage from './pages/Contact-Page/contactpage.js';
 import AboutPage from './pages/About-Page/aboutpage.js';
-import NotFound from './pages/notfoundpage.js';
 import RCName from './pages/Registration-Page/Registration.js';
-import helpTip from './components/helpicon.js';
-import homepage from './pages/Homepage/Homepage.js';
 import Address from './components/address.js';
 import history from './pages/History-Page/historicalplaces.js';
-import Login from './pages/Login-Page/loginpage.js';
 import Profiledd from './components/profiledd.js';
 import Profile from './pages/Profile-Page/Profile.js';
+import helpTip from './components/helpicon.js';
+import NotFound from './pages/notfoundpage.js';
+
 
 function App() {
 
@@ -23,12 +25,12 @@ function App() {
                 <Route exact path='/login' component={Login}/>
                 <Route exact path='/contact' component={ContactPage} />
                 <Route exact path='/about' component={AboutPage} />
-                <Route exact path='/helpicon' component={helpTip} />
                 <Route exact path='/name' component={RCName} />
                 <Route exact path='/address' component={Address} />
                 <Route exact path='/historicalplaces' component={history} />
                 <Route exact path='/profiledd' component={Profiledd} />
                 <Route exact path='/profile' component={Profile} />
+                <Route exact path='/helpicon' component={helpTip} />
                 <Route exact path='/404' component={NotFound} />
                 <Redirect to='/404' />
             </Switch>
