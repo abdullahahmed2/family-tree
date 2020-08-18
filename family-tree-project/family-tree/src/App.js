254ed00c7116c348502da8ed66090b2422db7367
Usama Ameen Ahmed <usamaameen@Usamas-MacBook-Pro.local>
2020-08-18 20:55:45 +0500

    .

--- family-tree-project/family-tree/src/App.js
+++ family-tree-project/family-tree/src/App.js
@@ -10,6 +10,7 @@ import Address from './components/Address-Component/address.js';
 import history from './pages/History-Page/historicalplaces.js';
 import Profiledd from './components/Profile-DropDown-Component/profiledd.js';
 import Profile from './pages/Profile-Page/Profile.js';
+import myFamily from './pages/Profile-Page/myfamily.js';
 import helpTip from './components/HelpIcon-Component/helpicon.js';
 import NotFound from './pages/notfoundpage.js';
 import Country from './components/Country-DropDown-Component/country.js';
@@ -30,6 +31,7 @@ function App() {
                 <Route exact path='/historicalplaces' component={history} />
                 <Route exact path='/profiledd' component={Profiledd} />
                 <Route exact path='/profile' component={Profile} />
+                <Route exact path='/myfamily' component={myFamily} />
                 <Route exact path='/helpicon' component={helpTip} />
                 <Route exact path='/country' component={Country} />
                 <Route exact path='/forgot' component={ForgotID} />
